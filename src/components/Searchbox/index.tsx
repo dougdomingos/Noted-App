9c078ecf6b2d4b6a0647b7ec6206977b1579e72b
Douglas Domingos <doug.domingossilva@gmail.com>
2022-09-14 14:34:35 -0300

    feat(searchbox): create searchbox component structure

--- src/components/Searchbox/index.tsx
+++ src/components/Searchbox/index.tsx
@@ -1,5 +1,26 @@
+import { Box, Flex, Icon, Input } from "@chakra-ui/react";
+import { HiSearch } from "react-icons/hi";
+
 function Searchbox() {
-  return null;
+  return (
+    <Box w={"full"} h={14} bgColor={"gray.600"} rounded={"md"}>
+      <Flex
+        h={"full"}
+        px={5}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        gap={4}
+      >
+        <Icon as={HiSearch} fontSize={"3xl"}/>
+        <Input
+          type={"text"}
+          variant={"unstyled"}
+          fontSize={"md"}
+          placeholder={"Search notes..."}
+        />
+      </Flex>
+    </Box>
+  );
 }
 
 export default Searchbox;

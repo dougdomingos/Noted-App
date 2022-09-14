2727a1ae63d79e58efd5a22469fa4e7e0719064e
Douglas Domingos <doug.domingossilva@gmail.com>
2022-09-14 14:06:22 -0300

    feat: add app base component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Center, Stack } from "@chakra-ui/react";
+
 function App() {
-  return <h1>Hello, React!</h1>
+  return (
+    <Center h={"100vh"}>
+      <Stack
+        bgColor={"gray.700"}
+        w={"90%"}
+        h={"95vh"}
+        maxW={"440px"}
+        rounded={"3xl"}
+      ></Stack>
+    </Center>
+  );
 }
 
 export default App;

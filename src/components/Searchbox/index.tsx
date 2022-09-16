3f5311e7673d55152c072647d25ab013aaf85fb2
Douglas Domingos <doug.domingossilva@gmail.com>
2022-09-16 07:41:23 -0300

    feat(Searchbox): change Searchbox styling

--- src/components/Searchbox/index.tsx
+++ src/components/Searchbox/index.tsx
@@ -1,24 +1,27 @@
-import { Box, Flex, Icon, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Icon,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+
 import { HiSearch } from "react-icons/hi";
 
 function Searchbox() {
   return (
     <Box w={"full"} h={14} bgColor={"gray.600"} rounded={"md"}>
-      <Flex
-        h={"full"}
-        px={5}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        gap={4}
-      >
-        <Icon as={HiSearch} fontSize={"3xl"} />
+      <InputGroup h={"full"} px={5}>
         <Input
           type={"text"}
           variant={"unstyled"}
-          fontSize={"md"}
+          fontSize={"lg"}
           placeholder={"Search notes..."}
         />
-      </Flex>
+        <InputRightElement h={"full"} mr={5}>
+          <Icon as={HiSearch} boxSize={7} />
+        </InputRightElement>
+      </InputGroup>
     </Box>
   );
 }

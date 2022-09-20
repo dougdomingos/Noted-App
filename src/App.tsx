c6b493be0495982a04dccae05a1576282d16f787
Douglas Domingos <doug.domingossilva@gmail.com>
2022-09-20 08:03:21 -0300

    feat(Toolbar): replace 'ActionsToggle' menu with Toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import { Center, Stack, useColorModeValue, useDisclosure } from "@chakra-ui/react";
+import {
+  Center,
+  Stack,
+  useColorMode,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 import Searchbox from "./components/Searchbox";
 import NoteList from "./components/NoteList";
 import NoteModal from "./components/NoteModal";
-import ActionsToggle from "./components/ActionsToggle";
+import ToolBar from "./components/Toolbar";
+
+import { TToolbar } from "./components/Toolbar";
 
 function App() {
+  const { toggleColorMode } = useColorMode();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  
+
   const backgroundColor = useColorModeValue("gray.50", "gray.800");
-  const appColor = useColorModeValue("gray.200", "gray.700")
+  const appColor = useColorModeValue("gray.200", "gray.700");
+
+  const triggers: TToolbar = {
+    triggerNewNote: onOpen,
+    triggerSetTheme: toggleColorMode,
+  };
 
   return (
     <>
@@ -26,7 +40,7 @@ function App() {
         >
           <Searchbox />
           <NoteList />
-          <ActionsToggle triggerNoteModal={onOpen} />
+          <ToolBar {...triggers} />
         </Stack>
       </Center>
       <NoteModal modalState={isOpen} closeModal={onClose} />
---
+++ src/components/Toolbar/index.tsx
@@ -0,0 +1,59 @@
+import {
+  Box,
+  Flex,
+  Stack,
+  Icon,
+  Text,
+  useColorModeValue,
+  Button,
+} from "@chakra-ui/react";
+
+import { IconType } from "react-icons";
+
+import { HiPlusCircle, HiMoon, HiSun } from "react-icons/hi";
+
+type TTolbarAction = {
+  icon: IconType;
+  label: string;
+  action: () => void;
+};
+
+export type TToolbar = {
+  triggerNewNote: () => void;
+  triggerSetTheme: () => void;
+};
+
+const ToolbarAction = ({ icon, label, action }: TTolbarAction) => (
+  <Button h={"full"} variant={"unstyled"} onClick={action}>
+    <Stack align={"center"}>
+      <Icon as={icon} boxSize={8} />
+      <Text maxW={"10ch"} fontSize={"sm"} textAlign={"center"}>
+        {label}
+      </Text>
+    </Stack>
+  </Button>
+);
+
+
+function Toolbar({ triggerNewNote, triggerSetTheme }: TToolbar) {
+  const themeActionIcon = useColorModeValue(HiMoon, HiSun);
+
+  return (
+    <Box>
+      <Flex h={16} justifyContent={"space-around"}>
+        <ToolbarAction
+          icon={HiPlusCircle}
+          label={"New Note"}
+          action={triggerNewNote}
+        />
+        <ToolbarAction
+          icon={themeActionIcon}
+          label={"Set theme"}
+          action={triggerSetTheme}
+        />
+      </Flex>
+    </Box>
+  );
+}
+
+export default Toolbar;

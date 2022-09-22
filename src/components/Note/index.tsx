23b0445941b9b8e9270c8fadafd19b98c87bc166
Douglas Domingos <doug.domingossilva@gmail.com>
2022-09-22 07:19:21 -0300

    feat(Note): remove delete button from Note

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -1,45 +1,30 @@
-import { Box, Flex, Stack, IconButton, Text, useColorModeValue } from "@chakra-ui/react";
-import { HiTrash } from "react-icons/hi";
+import { Box, Flex, Stack, Text, useColorModeValue } from "@chakra-ui/react";
 
 export type TNote = {
   id?: number;
   title: string;
   content: string;
-  triggerDelete?: () => void;
 };
 
-export function Note({ title, content, triggerDelete }: TNote) {
+export function Note({ title, content }: TNote) {
   const noteBgColor = useColorModeValue("gray.50", "gray.600");
 
   return (
     <Box
       pos={"relative"}
-      w={"full"}
+      h={56}
       bgColor={noteBgColor}
       px={5}
       py={4}
       rounded={"md"}
     >
       <Flex justifyContent={"space-between"} alignItems={"center"}>
-        <Stack maxW={"85%"}>
+        <Stack>
           <Text as={"span"} fontWeight={600} fontSize={"lg"}>
             {title}
           </Text>
-          <Text
-            textOverflow={"ellipsis"}
-            overflow={"hidden"}
-            whiteSpace={"nowrap"}
-          >
-            {content}
-          </Text>
+          <Text overflow={"hidden"}>{content}</Text>
         </Stack>
-        <IconButton
-          aria-label='Delete note'
-          fontSize={"2xl"}
-          variant={"ghost"}
-          icon={<HiTrash />}
-          onClick={triggerDelete}
-        />
       </Flex>
     </Box>
   );

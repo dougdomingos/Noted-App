e0faa70f383324b21479fc6de302c2d95dd68c5b
Douglas Domingos <doug.domingossilva@gmail.com>
2022-09-22 07:20:28 -0300

    feat(NoteList): remove 'DeleteNote' modal instance

--- src/components/NoteList/index.tsx
+++ src/components/NoteList/index.tsx
@@ -1,13 +1,10 @@
-import { SimpleGrid, useDisclosure } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
 import { generateNoteMocks } from "../../utils/noteMocks";
-import DeleteNoteModal from "../DeleteNoteModal";
 import { Note, TNote } from "../Note";
 
 import "./NoteList.styles.css";
 
 function NoteList() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   const notes: TNote[] = generateNoteMocks(6);
 
   return (
@@ -16,14 +13,8 @@ function NoteList() {
       gap={1.5}
       columns={{ base: 1, sm: 2, lg: 3 }}
     >
-      <DeleteNoteModal modalState={isOpen} closeModal={onClose} />
       {notes.map((note) => (
-        <Note
-          key={note.id}
-          title={note.title}
-          content={note.content}
-          triggerDelete={onOpen}
-        />
+        <Note key={note.id} title={note.title} content={note.content} />
       ))}
     </SimpleGrid>
   );

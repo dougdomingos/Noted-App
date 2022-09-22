ed30a495f13b05832a32f744be0cf8c5ca903ade
Douglas Domingos <doug.domingossilva@gmail.com>
2022-09-22 07:25:59 -0300

    feat(NoteList): wrap NoteList grid with Box component

--- src/components/NoteList/index.tsx
+++ src/components/NoteList/index.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Box } from "@chakra-ui/react";
 import { generateNoteMocks } from "../../utils/noteMocks";
 import { Note, TNote } from "../Note";
 
@@ -8,15 +8,16 @@ function NoteList() {
   const notes: TNote[] = generateNoteMocks(6);
 
   return (
-    <SimpleGrid
-      overflowY={"auto"}
-      gap={1.5}
-      columns={{ base: 1, sm: 2, lg: 3 }}
-    >
-      {notes.map((note) => (
-        <Note key={note.id} title={note.title} content={note.content} />
-      ))}
-    </SimpleGrid>
+    <Box h={"full"} overflowY={"auto"}>
+      <SimpleGrid
+        gap={2.5}
+        columns={{ base: 1, sm: 2, lg: 3 }}
+      >
+        {notes.map((note) => (
+          <Note key={note.id} title={note.title} content={note.content} />
+        ))}
+      </SimpleGrid>
+    </Box>
   );
 }
 

65a7eead672836bda8bf18d4944df915385fe839
Douglas Domingos <doug.domingossilva@gmail.com>
2022-10-02 07:31:33 -0300

    feat(Note): add options menu button to note

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -5,8 +5,15 @@ import {
   Text,
   useColorModeValue,
   useBreakpointValue,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useMediaQuery,
 } from "@chakra-ui/react";
 
+import { useState } from "react";
+import { HiDotsHorizontal, HiTrash, HiPencil } from "react-icons/hi";
 import { trimText } from "../../utils/trimText";
 
 export type TNote = {
@@ -15,8 +22,28 @@ export type TNote = {
   content: string;
 };
 
+const NoteOptionsMenu = () => {
+  return (
+    <Box pos={"absolute"} top={4} right={5}>
+      <Menu placement={"left-start"}>
+        <MenuButton>
+          <HiDotsHorizontal size={24} />
+        </MenuButton>
+        <MenuList>
+          <MenuItem icon={<HiPencil size={20} />}>Edit</MenuItem>
+          <MenuItem icon={<HiTrash size={20} />}>Delete</MenuItem>
+        </MenuList>
+      </Menu>
+    </Box>
+  );
+};
+
 export function Note({ title, content }: TNote) {
   const noteBgColor = useColorModeValue("gray.100", "gray.700");
+
+  // for options menu display
+  const [showOptionsButton, setShowOptionsButton] = useState(false);
+  const [isSmallerThanLg] = useMediaQuery("(max-width: 62em)");
 
   const contentMaxLength = useBreakpointValue({
     base: 120,
@@ -34,6 +61,8 @@ export function Note({ title, content }: TNote) {
       px={5}
       py={4}
       rounded={"md"}
+      onMouseEnter={() => setShowOptionsButton(true)}
+      onMouseLeave={() => setShowOptionsButton(false)}
     >
       <Flex justifyContent={"space-between"} alignItems={"center"}>
         <Stack>
@@ -43,6 +72,7 @@ export function Note({ title, content }: TNote) {
           <Text>{trimmedText}</Text>
         </Stack>
       </Flex>
+      {(isSmallerThanLg || showOptionsButton) && <NoteOptionsMenu />}
     </Box>
   );
 }
